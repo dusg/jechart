b1813f3dc3590c6e5812a5acea1b3bfc60310ed9
dusg <siguang_du@c-sky.com>
2020-10-10 16:52:08 +0800

    add setOption function

--- src/main/java/org/dusg/jechart/JEChart.java
+++ src/main/java/org/dusg/jechart/JEChart.java
@@ -1,12 +1,23 @@
 package org.dusg.jechart;
 
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.browser.BrowserFunction;
+import org.eclipse.swt.browser.ProgressListener;
+import org.eclipse.swt.widgets.Composite;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JEChart {
     private static Path extraPath;
+    private final Browser browser;
+    private Object option = new HashMap<String, Object>();
+    protected boolean loadComplete = false;
+    private final JEChart.getOptionFunction getOptionFunction;
 
     public static Path getExtraPath() {
         return extraPath;
@@ -35,4 +46,89 @@ public class JEChart {
         extraResources();
     }
 
+    public JEChart(Composite parent, int style) {
+        browser = new Browser(parent, style);
+        browser.setUrl("file://" + JEChart.getExtraPath() + "/web/index.html");
+        browser.addProgressListener(ProgressListener.completedAdapter(progressEvent -> {
+            setLoadComplete(true);
+            jsSetOption();
+        }));
+        getOptionFunction = new getOptionFunction(browser);
+
+        browser.addDisposeListener(disposeEvent -> {
+            getOptionFunction.dispose();
+        });
+    }
+
+    private void jsSetOption() {
+        javaToJs(option);
+        browser.execute(String.format("chart.setOption(%s)", javaToJs(getOption())));
+    }
+
+    private String javaToJs(Object option) {
+        if (option instanceof String) {
+            return String.format("'%s'", option);
+        }
+        if (option instanceof Number) {
+            Number number = (Number) option;
+            return number.toString();
+        }
+        if (option instanceof Object[]) {
+            Object[] objects = (Object[]) option;
+            StringBuilder builder = new StringBuilder();
+            builder.append("[");
+            for (Object object : objects) {
+                builder.append(javaToJs(object));
+                builder.append(",");
+            }
+            builder.append("]");
+            return builder.toString();
+        }
+        if (option instanceof Map) {
+            Map map = (Map) option;
+            StringBuilder builder = new StringBuilder();
+            builder.append("{");
+
+            map.forEach((key, value) -> {
+                builder.append(javaToJs(key));
+                builder.append(":");
+                builder.append(javaToJs(value));
+                builder.append(",");
+            });
+            builder.append("}");
+            return builder.toString();
+        }
+        return "undefined";
+    }
+
+    public Object getOption() {
+        return option;
+    }
+
+    public void setOption(Object option) {
+        this.option = option;
+        if (isLoadComplete()) {
+            jsSetOption();
+        }
+    }
+
+    public boolean isLoadComplete() {
+        return loadComplete;
+    }
+
+    protected void setLoadComplete(boolean loadComplete) {
+        this.loadComplete = loadComplete;
+    }
+
+    class getOptionFunction extends BrowserFunction {
+
+        public getOptionFunction(Browser browser) {
+            super(browser, "getOption");
+        }
+
+        @Override
+        public Object function(Object[] arguments) {
+            return getOption();
+        }
+    }
 }
--- src/test/java/org/dusg/jechart/snapshot/SnippetBar.java
+++ src/test/java/org/dusg/jechart/snapshot/SnippetBar.java
@@ -2,9 +2,13 @@ package org.dusg.jechart.snapshot;
 
 import org.dusg.jechart.JEChart;
 import org.eclipse.swt.*;
-import org.eclipse.swt.browser.*;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
+
+import java.util.AbstractMap;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SnippetBar {
     public static void main(String[] args) {
@@ -12,16 +16,24 @@ public class SnippetBar {
         Shell shell = new Shell(display);
         shell.setText("Snippet 136");
         shell.setLayout(new FillLayout());
-        Browser browser;
+        JEChart browser;
         try {
-            browser = new Browser(shell, SWT.NONE);
+            browser = new JEChart(shell, SWT.NONE);
+            HashMap<String, Object> option = new HashMap<>();
+            option.put("title", Map.of("text", "ECharts 入门示例"));
+            option.put("tooltip", Collections.emptyMap());
+            option.put("yAxis", Collections.emptyMap());
+            option.put("legend", Map.of("data", new String[]{"销量"}));
+            option.put("xAxis", Map.of("data", new String[]{"衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"}));
+            option.put("series", new Object[]{Map.of("name", "销量", "type", "bar", "data", new Object[]{5, 20, 36, 10, 10
+                    , 20})});
+            browser.setOption(option);
         } catch (SWTError e) {
             System.out.println("Could not instantiate Browser: " + e.getMessage());
             display.dispose();
             return;
         }
 
-        browser.setUrl("file://" + JEChart.getExtraPath() + "/web/index.html");
         shell.open();
         while (!shell.isDisposed()) {
             if (!display.readAndDispatch())
